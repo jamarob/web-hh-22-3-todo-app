493a985ead709d77283a900f6aa91192e7662e72
jamarob <janmartinrobert@gmail.com>
2022-06-20 11:20:28 +0200

    Add error handling for validation error

--- api/todos/[id].js
+++ api/todos/[id].js
@@ -1,47 +1,51 @@
 const enclosedHandler = async (request, response) => {
-  await connectToMongodb()
+  try {
+    await connectToMongodb()
 
-  const { id } = request.query
+    const { id } = request.query
 
-  const { method } = request
+    const { method } = request
 
-  if (method === 'GET') {
-    const foundTodo = await Todo.findById(id)
-    if (!foundTodo) {
-      return response.status(404).send()
+    if (method === 'GET') {
+      const foundTodo = await Todo.findById(id)
+      if (!foundTodo) {
+        return response.status(404).send()
+      }
+      return response.status(200).json(foundTodo)
     }
-    return response.status(200).json(foundTodo)
-  }
 
-  if (method === 'DELETE') {
-    const updatedTodo = await Todo.findByIdAndUpdate(id, request.body, {
-      new: true,
-    })
-    if (!updatedTodo) {
-      return response.status(404).send()
+    if (method === 'DELETE') {
+      const updatedTodo = await Todo.findByIdAndUpdate(id, request.body, {
+        new: true,
+      })
+      if (!updatedTodo) {
+        return response.status(404).send()
+      }
+      return response.status(200).json(updatedTodo)
     }
-    return response.status(200).json(updatedTodo)
-  }
 
-  if (method === 'PUT') {
-    const todoToUpdate = await Todo.findById(id)
-    if (!todoToUpdate) {
-      return response.status(404).send()
+    if (method === 'PUT') {
+      const todoToUpdate = await Todo.findById(id)
+      if (!todoToUpdate) {
+        return response.status(404).send()
+      }
+      todoToUpdate.description = request.body.description
+      todoToUpdate.done = request.body.done
+      const updatedTodo = await todoToUpdate.save()
+      return response.status(200).json(updatedTodo)
     }
-    todoToUpdate.description = request.body.description
-    todoToUpdate.done = request.body.done
-    const updatedTodo = await todoToUpdate.save()
-    return response.status(200).json(updatedTodo)
-  }
 
-  if (method === 'PATCH') {
-    const updatedTodo = await Todo.findByIdAndUpdate(id, request.body, {
-      new: true,
-    })
-    if (!updatedTodo) {
-      return response.status(404).send()
+    if (method === 'PATCH') {
+      const updatedTodo = await Todo.findByIdAndUpdate(id, request.body, {
+        new: true,
+      })
+      if (!updatedTodo) {
+        return response.status(404).send()
+      }
+      return response.status(200).json(updatedTodo)
     }
-    return response.status(200).json(updatedTodo)
+  } catch (error) {
+    return response.status(400).json(error)
   }
 
   response.status(405).send()
